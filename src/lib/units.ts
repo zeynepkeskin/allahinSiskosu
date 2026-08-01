9a1961f5c2011b11b7690ff90708bce1d92cfd63
Zeynep <zeynepkeskin10@gmail.com>
2026-08-01 02:01:35 -0700

    round numbers

--- src/lib/units.ts
+++ src/lib/units.ts
@@ -1,7 +1,10 @@
 export const KG_PER_LB = 0.45359237;
 export const CM_PER_IN = 2.54;
 
+export const roundToOneDecimal = (value: number) => Math.round(value * 10) / 10;
 export const poundsToKilograms = (pounds: number) => pounds * KG_PER_LB;
-export const kilogramsToPounds = (kilograms: number) => kilograms / KG_PER_LB;
+export const kilogramsToPounds = (kilograms: number) =>
+  roundToOneDecimal(kilograms / KG_PER_LB);
 export const inchesToCentimeters = (inches: number) => inches * CM_PER_IN;
-export const centimetersToInches = (centimeters: number) => centimeters / CM_PER_IN;
+export const centimetersToInches = (centimeters: number) =>
+  roundToOneDecimal(centimeters / CM_PER_IN);
